80bd1062fa36a454f235cf2cccee1f99478c94e5
bartosh17 <bartkon2006@gmail.com>
2024-01-07 19:49:50 +0000

    visual fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,76 +1,80 @@
 <script lang="ts">
 	// Skrypt do strony
-    let boardState = [];
-
-    for (let i = 0; i < 3; i++) {
-        boardState.push(['', '', ''])
-    }
-
-    console.log(boardState)
-</script>
-
-<script lang="ts">
-    // Skrypt do strony
     let boardState = [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
     ];
 
-    counter = 0;
-
-    int counter = 0;
-    int gameState = ''
+    let counter = 0;
+    let gameState = "";
 
     function checkWin() {
         for (let i = 0; i < 3; i += 1) {
-
             if (boardState[0][i] !== "" &&
-                boardState[0][i] === boardState[1][i] &&
-                boardState[1][i] === boardState[2][i]) {
-                    return true;
-            }
-
-            if (boardState[i][0] !== "" &&
-                boardState[i][0] === boardState[i][1] &&
-                boardState[i][1] === boardState[i][2]) {
-                    return true;
-                }
-            }
-
-            if (boardState[0][0] !== "" &&
-                boardState[0][0] === boardState[1][1] &&
-                boardState[1][1] === boardState[2][2]) {
-                    return true;
-            }
-
-            if (boardState[0][2] !== "" &&
-                boardState[0][2] === boardState[1][1] &&
-                boardState[1][1] === boardState[2][0]) {
+            boardState[0][i] === boardState[1][i] &&
+            boardState[1][i] === boardState[2][i]) {
                 return true;
             }
 
-            return false;
+            if (boardState[i][0] !== "" &&
+            boardState[i][0] === boardState[i][1] &&
+            boardState[i][1] === boardState[i][2]) {
+                return true;
+            }
+        }
+
+        if (boardState[0][0] !== "" &&
+        boardState[0][0] === boardState[1][1] &&
+        boardState[1][1] === boardState[2][2]) {
+            return true;
+        }
+
+        if (boardState[0][2] !== "" &&
+        boardState[0][2] === boardState[1][1] &&
+        boardState[1][1] === boardState[2][0]) {
+            return true;
+        }
+        return false;
     }
 
-    function markSquare(x: number, y: number)
-        if (boardState[y][x] !== "") {
+    function markSquare(x: number, y: number) {
+        if (boardState[y][x] !== "" || gameState !== "") {
             return;
         }
 
-        {
         if (counter % 2 === 0) {
-            boartState[y][x] \ 'O'
-        }
-        else {
-            boartState[y][x] = 'X'
+            boardState[y][x] = "O";
+        } else {
+            boardState[y][x] = "X";
         }
 
-        checkWin();
-
-        counter += 1
+        const isWin = checkWin();
+        if (isWin) {
+            if (counter % 2 === 0) {
+                gameState = "O";
+            } else {
+                gameState = "X";
+            }
         }
-        resetGame()
+        
+        counter += 1;
+
+        if (counter === 9) {
+            gameState = "OX";
+        }
+    }
+
+    function resetGame() {
+        boardState = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+        
+        counter = 0;
+        gameState = "";
+    }
 </script>
 
 <main>
@@ -79,41 +83,63 @@
         {#each [0, 1, 2] as y}
             <tr>
                 {#each [0, 1, 2] as x}
-                    <td class="cell">
+                    <td class="cell" on:click={() => {markSquare(x, y)}}>
                         {boardState[y][x]}
+                        <!-- ({x}, {y}) -->
                     </td>
                 {/each}
             </tr>
         {/each}
     </table>
-    {counter}
-
-    {#if gameState === 'O'}
+    
+    {#if gameState === "O"}
         O wins
-    {:else if gameState === 'OX'}
+    {:else if gameState === "X"}
+        X wins
+    {:else if gameState === "OX"}
         Draw
     {:else}
-        Turn
-        {if counter % 2  === 0}
+        Turn:
+        {#if counter % 2 === 0}
             O
         {:else}
             X
         {/if}
+    {/if}
 
-    <button on: click=resetGame> Game over</button>
+    <button on:click={resetGame}>Play again</button>
 </main>
 
 <style>
     /* Styl strony */
+    table {
+        margin-left: auto;
+        margin-right: auto;
+        caret-color: transparent;
+        background-color: white;
+    }
+
     .cell {
         width: 100px;
         height: 100px;
-        background-color: aqua;
-        color: black;
+        background-color: black;
+        color: white;
         text-align: center;
-        font-size: 40px;
+        font-size: 60px;
     }
 
-    main:
-        font-size: 20px;
+    .cell:hover {
+        cursor: pointer;
+    }
+
+    main {
+        font-size: 50px;
+        background-color: black;
+    }
+    
+    button {
+        font-size: 50px;
+        margin-left: auto;
+        margin-right: auto;
+    }
 </style>
